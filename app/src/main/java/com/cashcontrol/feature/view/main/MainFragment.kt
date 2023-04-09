b6e5dbc99922f86086fe2044796693d2b6ce0a4d
Andrii Khalymendyk <aerys.prog@gmail.com>
2023-04-09 14:51:31 +0300

    implemented animation and refactored

--- app/src/main/java/com/cashcontrol/feature/view/main/MainFragment.kt
+++ app/src/main/java/com/cashcontrol/feature/view/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.cashcontrol.feature.view.main
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
@@ -14,7 +15,6 @@ import com.cashcontrol.domain.base.BaseFragment
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
 class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
 
     var isStateDelayScrolled = false
@@ -24,15 +24,19 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.svParent.isVerticalScrollBarEnabled = false
+        binding.svParent.setOnTouchListener { _, _ -> true }
+
         binding.rvTransactionList.layoutManager = LinearLayoutManager(context)
         binding.rvTransactionList.adapter = TransactionListAdapter(emptyList())
 
         val snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(binding.rvTransactionList)
 
-        binding.svParent.isVerticalScrollBarEnabled = false
-        binding.svParent.setOnTouchListener { _, _ -> true }
+        setListeners()
+    }
 
+    private fun setListeners() {
         binding.rvTransactionList.addOnScrollListener(object : OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 if (!isStateDelayScrolled) {
@@ -43,14 +47,14 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
                     //scroll up
                     if (dy > COORDINATE_ONE && currentItemPosition > SECOND_POSITION && !isStateDelayManyTouchDown) {
                         isStateDelayManyTouchDown = true
-                        binding.svParent.smoothScrollTo(FIRST_POSITION, binding.textView2.top)
+                        animate(binding.svParent, binding.textView2.top)
                         lifecycleScope.launch {
                             delay(MANY_TOUCH_DELAY)
                             isStateDelayManyTouchDown = false
                         }
                         //scroll down
                     } else if (dy < COORDINATE_ZERO && currentItemPosition in FIRST_POSITION..SECOND_POSITION) {
-                        binding.svParent.smoothScrollTo(FIRST_POSITION, binding.svParent.top)
+                        animate(binding.svParent, binding.svParent.top)
                     }
 
                     lifecycleScope.launch {
@@ -61,15 +65,20 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
                 }
             }
         })
+
         binding.textView2.setOnClickListener {
             isStateDelayScrolled = true
-            binding.svParent.smoothScrollTo(FIRST_POSITION, binding.svParent.top)
+            animate(binding.svParent, binding.svParent.top)
             binding.rvTransactionList.smoothScrollToPosition(FIRST_POSITION)
             lifecycleScope.launch {
                 delay(MANY_TOUCH_DELAY)
                 isStateDelayScrolled = false
             }
         }
+    }
+
+    private fun animate(view: View, positionY:Int, propertyName:String = "scrollY"){
+        ObjectAnimator.ofInt(view, propertyName,  positionY).setDuration(ANIMATION_DELAY).start()
     }
 
 
@@ -81,5 +90,6 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
         private const val COORDINATE_ONE = 1
 
         private const val MANY_TOUCH_DELAY = 150L
+        private const val ANIMATION_DELAY = 190L
     }
 }
